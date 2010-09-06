83c61471e8c71be89a86254e9d7a3d611a06eb7f
Jordan M <jordan@meepmail.co.uk>
2010-09-06 22:33:00 +0100

    Wrapper 'links' in raw to prevent rails escaping the content. Works on rails 3.

--- lib/navigation.rb
+++ lib/navigation.rb
@@ -54,7 +54,7 @@ module RPH
         end
         
         return '' if items.blank?
-        content_tag(:ul, items, :class => 'navigation')
+        content_tag(:ul, raw(items), :class => 'navigation')
       end
       
       private
